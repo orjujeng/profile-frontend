a397e9da5a3d274bbab6268870edcdf4cf7abf83
orjujeng <orjujeng@hotmail.com>
2025-04-04 00:28:22 +0800

    change the index file

---
+++ index.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom';
+import App from './src/components/App'
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
---
+++ src/components/App/index.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from 'react';
+function App() {
+  return (
+    <Fragment>
+      <h1>App components</h1>
+    </Fragment>
+  )
+}
+export default App
