2056c9eb79eb161d7c74c23a7bc4d3d27c5a08a0
orjujeng <orjujeng@hotmail.com>
2025-04-13 21:44:45 +0800

    title pc

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,26 +2,29 @@ import React from 'react'
 import List from '../../public_components/List/List'
 import Brand from '../../public_components/Brand/Brand'
 import CapsuleButton from '../../public_components/capsuleButton/capsuleButton'
+import styles from '../../styles/Title.module.scss'
 export default function Title() {
   const authInfo = {
-    name : 'Contact me',
+    name : 'Login',
     image : ''
   }
   return (
     <>
+    <div className={styles.titleWarp}>
       {/* all route page with function*/}
-      <div className="routeList">
+      <div className={styles.routeList}>
         <List names={['Wokers','Service','About']}/>
       </div>
       {/* logo */}
-      <div className="brand">
+      <div className={styles.brand}>
         <Brand wording = "orjujeng"/>
       </div>
       {/* info and auth */}
-      <div className="authInfo">
+      <div className={styles.authInfo}>
         <CapsuleButton data = {authInfo}/>
       </div>
       {/* auth entrance */}
+    </div>
     </>
   )
 }
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Title from '../../components/Title/Title'
+import styles from '../../styles/Profile.module.scss'
 export default function profile() {
   return (
     <>
+    <div className={styles.profileTitleWarp}>
       <Title/>
+    </div>
     </>
   )
 }
