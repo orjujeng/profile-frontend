93b65c0bd2a2229221896d4bc6d32d3b4550a794
orjujeng <orjujeng@hotmail.com>
2025-04-28 23:05:55 +0800

    with issue of img

--- src/components/Frontend/Frontend.jsx
+++ src/components/Frontend/Frontend.jsx
@@ -6,28 +6,30 @@ import SubTitle from '../../public_components/SubTitle/SubTitle';
 import Description from '../../public_components/Description/Description';
 import Tag from '../../public_components/Tag/Tag';
 import CapsuleButton from '../../public_components/capsuleButton/capsuleButton';
+import img from '../../assets/png/mainpage-1-desktop-C6lInk3R.webp'
 export default function Frontend() {
-    const { width } = checkWindowSize();
-    const isMobile = width <= 1024;
-    const isMicro = width <= 766;
+  const { width } = checkWindowSize();
+  const isMobile = width <= 1024;
+  const isMicro = width <= 766;
 
-    const repoInfo= {
-      name: 'Code Repository',
-      style : 'light',
-      image :'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png'
-    }
-    const siteInfo= {
-      name: 'View Online',
-      image :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE84H4meueuyC578lSCHyl8LwonoKP5xylTg&s',
-    }
+  const repoInfo = {
+    name: 'Code Repository',
+    style: 'light',
+    image: 'https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png'
+  }
+  const siteInfo = {
+    name: 'View Online',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE84H4meueuyC578lSCHyl8LwonoKP5xylTg&s',
+  }
   return (
     <>
-    <div  className={isMicro ? styles.frontendWarpMicro : styles.frontendWarp}>
-    {/*  header */}
-        <Header title='WORK'/>
-    {/* web port */}
-    <div  className={isMobile ? styles.mainWarpApp: styles.mainWarp}>
-     {isMobile ? 111 : <div className={styles.infoPart}>
+      <div className={isMicro ? styles.frontendWarpMicro : styles.frontendWarp}>
+        {/*  header */}
+        <Header title='WORK' />
+        {/* web port */}
+        <div className={isMobile ? styles.mainWarpApp : styles.mainWarp}>
+          {isMobile ? 1111111111 : 
+     <div className={styles.infoPart}>
     {/* left desc */}
     {/* subh1 */}
     <SubTitle title = 'Orjujeng.com'/>
@@ -46,11 +48,17 @@ export default function Frontend() {
             {/* email button */}
         <CapsuleButton data = {siteInfo}/>
     </div>
-    {/* right pic */}
-     </div> }
-    
-    </div>
     </div> 
+    }
+    {/* right pic pc and mobile only*/}
+    {isMicro ? 11111111111 : 
+     <div className={styles.thumbnailWarp}>
+      {/* right pic */}
+      <img src={img} alt="" />
+    </div> 
+    }
+        </div>
+      </div>
     </>
   )
 }
