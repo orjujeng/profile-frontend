3dc258aa16e3bd7ee303c0aa0f7892e98fe3b396
orjujeng <orjujeng@hotmail.com>
2025-08-19 01:12:41 +0800

    progress design

--- src/public_components/Progress/Progress.jsx
+++ src/public_components/Progress/Progress.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import styles from '../../public_style/Progress.module.scss'
 import Printbox from '../Printbox/Printbox'
-export default function Progress() {
+export default function Progress(props) {
+  const { imgPath,title,projectPercent} = props;
   return (
     <>
       <Printbox>
-        {/* img */}
-        {/* title */}
-        {/* progress */}
-        {/*percent */}
+        <div className={styles.progressWarp}>
+          {/* img */}
+          <div className={styles.imgWarp}>
+            <img src={imgPath} />
+            11111
+          </div>
+          <div className={styles.textWarp}>
+            {/* title */}
+            <div className={styles.title}>
+              {title} Water retention
+            </div>
+            <div className={styles.projectProcess}>
+              {projectPercent} 11<span>/100% Project Process</span>
+            </div>
+            {/* progress */}
+            <div className={styles.progressWarp}>
+              <div className={styles.progress}></div>
+            </div>
+            {/*percent */}
+          </div>
+        </div>
       </Printbox>
     </>
   )
